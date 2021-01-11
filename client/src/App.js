d00de1ab4c520a4ea20ad95471ecda9915c71e96
ky-lore <kclore3131@gmail.com>
2021-01-11 14:26:00 -0800

    working router and login signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,43 @@
+import SignUp from './Components/SignUp/SignUp.js'
+import SignIn from './Components/SignIn/SignIn.js'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">SignUp</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/register">
+            <SignUp />
+          </Route>
+          <Route path="/login">
+            <SignIn />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
