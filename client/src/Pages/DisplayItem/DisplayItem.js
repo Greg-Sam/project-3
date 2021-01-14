741765579e7d732de98693f5dbcab985772ebfa9
Amee Parmar <amee05@yahoo.com>
2021-01-14 14:22:41 -0800

    displayItem page- still dummy

--- client/src/Pages/DisplayItem/DisplayItem.js
+++ client/src/Pages/DisplayItem/DisplayItem.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Item from '../../utils/ItemAPI'
+// import Item from '../../utils/ItemAPI'
 import Navbar from '../../Components/Navbar/Navbar.js'
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import Axios from 'axios'
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardText, MDBCardTitle, MDBBtn } from "mdbreact";
+// import Axios from 'axios'
 
 
-const {
-  getDisplayItem
-} = getItem
-
+let item = {
+  name: 'Canon EOS 90D DSLR Camera',
+  description: 'Canon EOS 90D DSLR Camera + 18-55mm STM + 70-300mm + 30 Piece Accessory Bundle Canon EOS 90D DSLR Camera',
+  price: '$800',
+  condition: 'Used-Good'
+}
 const DisplayItem = id => {
   return (
     <>
@@ -16,8 +18,21 @@ const DisplayItem = id => {
       <Navbar />
       <MDBRow>
         <MDBRow>
-          <MDBCol>1 of 2</MDBCol>
-          <MDBCol>2 of 2</MDBCol>
+            <MDBCol> <img src="https://live.staticflickr.com/8014/7132453187_98abce3c17_b.jpg" className="img-fluid" alt="" /></MDBCol>
+            <MDBCol><MDBCard>
+              <MDBCardImage
+                className='blue-gradient white-text d-flex justify-content-center align-items-center flex-column p-4 rounded'
+                tag='div'
+              >
+                <h2>{item.name}</h2>
+                <p>{item.condition}</p>
+              </MDBCardImage>
+              <MDBCardBody cascade className='text-center'>
+                <MDBCardTitle>{item.price}</MDBCardTitle>
+                <MDBCardText>{item.description}</MDBCardText>
+                <MDBBtn color="primary" href="#">BUY</MDBBtn>
+              </MDBCardBody>
+            </MDBCard></MDBCol>
         </MDBRow>
       </MDBRow>
     </MDBContainer>
