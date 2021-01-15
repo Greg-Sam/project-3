50a9fba36457a8464046e072ce2143cd1f30b431
Wettim <wettimlinton123@gmail.com>
2021-01-15 12:19:57 -0800

    nodemon issues fixed

--- db/index.js
+++ db/index.js
@@ -2,3 +2,8 @@ module.exports = require('mongoose').connect(process.env.MONGODB_URI || process.
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
+
+// process.env.LOCAL_URI
+
+// 'mongodb://localhost/project3_db'
