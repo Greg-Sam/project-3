5ee105458490257b591429eed1ab662c72bdf47a
ky-lore <kclore3131@gmail.com>
2021-01-14 20:42:47 -0800

    display item page half working

--- client/src/Pages/DisplayItem/DisplayItem.js
+++ client/src/Pages/DisplayItem/DisplayItem.js
@@ -1,42 +1,60 @@
-import React from "react";
-// import Item from '../../utils/ItemAPI'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from "react"
+import { useEffect, useState } from 'react'
+import Item from '../../utils/ItemAPI/ItemAPI'
 import Navbar from '../../Components/Navbar/Navbar.js'
 import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardText, MDBCardTitle, MDBBtn } from "mdbreact";
 // import Axios from 'axios'
 
+const {
+  getItems
+} = Item
 
-let item = {
-  name: 'Canon EOS 90D DSLR Camera',
-  description: 'Canon EOS 90D DSLR Camera + 18-55mm STM + 70-300mm + 30 Piece Accessory Bundle Canon EOS 90D DSLR Camera',
-  price: '$800',
-  condition: 'Used-Good'
-}
 const DisplayItem = id => {
+  const [itemState, setItemState] = useState({
+    isLoaded: false,
+    items: []
+  })
+
+  useEffect(async () => {
+    let { data: item } = await getItems('Automotive')
+    setItemState({ ...itemState, isLoaded: true })
+    setItemState({ ...itemState, items: item })
+  }, [])
+
+  let fetchItem = itemState.items.filter(item => item._id === '5fff84574f1dd816eca21d31')
+
+  let fetchedItem = fetchItem[0]
+
   return (
     <>
-    <MDBContainer>
       <Navbar />
-      <MDBRow>
-        <MDBRow>
-            <MDBCol> <img src="https://live.staticflickr.com/8014/7132453187_98abce3c17_b.jpg" className="img-fluid" alt="" /></MDBCol>
-            <MDBCol><MDBCard>
-              <MDBCardImage
-                className='blue-gradient white-text d-flex justify-content-center align-items-center flex-column p-4 rounded'
-                tag='div'
-              >
-                <h2>{item.name}</h2>
-                <p>{item.condition}</p>
-              </MDBCardImage>
-              <MDBCardBody cascade className='text-center'>
-                <MDBCardTitle>{item.price}</MDBCardTitle>
-                <MDBCardText>{item.description}</MDBCardText>
-                <MDBBtn color="primary" href="#">BUY</MDBBtn>
-              </MDBCardBody>
-            </MDBCard></MDBCol>
-        </MDBRow>
-      </MDBRow>
-    </MDBContainer>
+      { console.log(fetchedItem) }
+      { itemState.items.length > 0 ?
+        <MDBContainer>
+          <MDBRow>
+            <MDBRow>
+              <MDBCol> <img src={fetchedItem.image} className="img-fluid" alt="" /></MDBCol>
+              <MDBCol><MDBCard>
+                <MDBCardImage
+                  className='blue-gradient white-text d-flex justify-content-center align-items-center flex-column p-4 rounded'
+                  tag='div'
+                >
+                  <h2>{fetchedItem.name}</h2>
+                  <p>{fetchedItem.condition}</p>
+                </MDBCardImage>
+                <MDBCardBody cascade className='text-center'>
+                  <MDBCardTitle>${fetchedItem.price}</MDBCardTitle>
+                  <MDBCardText>{fetchedItem.description}</MDBCardText>
+                  <MDBBtn color="primary" href="#">BUY</MDBBtn>
+                </MDBCardBody>
+              </MDBCard></MDBCol>
+            </MDBRow>
+          </MDBRow>
+        </MDBContainer>
+        : null}
     </>
+
   )
 }
 
