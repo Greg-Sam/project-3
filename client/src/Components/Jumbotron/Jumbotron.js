89882bbcd5aae8a952b70b277cd58ad9aaae2c9d
Amee Parmar <amee05@yahoo.com>
2021-01-21 12:09:03 -0800

    creating app.css

--- client/src/Components/Jumbotron/Jumbotron.js
+++ client/src/Components/Jumbotron/Jumbotron.js
@@ -6,7 +6,7 @@ const Jumbotron = () => {
     <MDBContainer fluid className="pl-0 pr-0">
       <MDBJumbotron fluid style={{ padding: 0 }}>
         <MDBCol className="text-white text-center py-2 px-4 my-0" style={{ backgroundImage: `url(https://i.ibb.co/7byV9VW/srbanner.png)` }}>
-          <MDBCardTitle className="h1-responsive pt-3 m-4 my-0 font-bold"></MDBCardTitle>
+          <MDBCardTitle className="h1-responsive pt-3 m-4 my-0 font-bold text-center"></MDBCardTitle>
           <p className="my-5"><br></br>
           <br></br>
           <br></br>
