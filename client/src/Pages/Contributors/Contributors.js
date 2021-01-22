45abea460615b2136df9c9cb8d321b0d70d64bab
Amee Parmar <amee05@yahoo.com>
2021-01-21 16:03:09 -0800

    linking the page with new icons

--- client/src/Pages/Contributors/Contributors.js
+++ client/src/Pages/Contributors/Contributors.js
@@ -17,9 +17,20 @@ const Contributors = () => {
          <hr></hr>
           <h4>Amee Parmar</h4>
           <hr></hr>
-          <a href="https://github.com/amee05"><MDBIcon fab icon="github" size="2x" /></a>
-          <a href="https://www.linkedin.com/in/amee-parmar-5b30a526/?trk=profile-badge-cta"> <MDBIcon fab icon="linkedin" size="2x" /></a>
-          <a href="mailto:amee05@yahoo.com"><MDBIcon icon="at" size="2x" /></a>
+            <a href="mailto:amee05@yahoo.com">
+              <img src="https://img.icons8.com/dusk/40/000000/email.png" alt="Email" />
+            </a>
+            <a href="https://github.com/amee05">
+
+              <img src="https://img.icons8.com/plasticine/50/000000/github.png" alt="GitHub" />
+            </a>
+          
+            <a href="https://www.linkedin.com/in/amee-parmar-5b30a526/?trk=profile-badge-cta">
+
+              <img src="https://img.icons8.com/doodle/40/000000/linkedin--v2.png"  alt="Linkedin" />
+            </a>
+            
+         
           <hr></hr>
           <h6>JavaScript, Node.js, SQL developer. Can speak three languages and has been to ten countries..Loking forward to visit many more. If I am not coding then I am reading.</h6>
         </MDBCol>
@@ -30,9 +41,21 @@ const Contributors = () => {
           <hr></hr>
           <h4>Greg Samuelson</h4>
           <hr></hr>
-          <a href="mailto:grssam@gmail.com"><MDBIcon far icon="envelope" size="2x" /></a>
-          <a href="https://github.com/Greg-Sam"><MDBIcon fab icon="github" size="2x" /></a>
-          <a href="https://www.linkedin.com/in/greg-samuelson/"> <MDBIcon fab icon="linkedin" size="2x" /></a>
+          {/* <a href="mailto:grssam@gmail.com"><MDBIcon far icon="envelope" size="2x" /></a> */}
+          {/* <a href="https://github.com/Greg-Sam"><MDBIcon fab icon="github" size="2x" /></a> */}
+            <a href="mailto:grssam@gmail.com">
+              <img src="https://img.icons8.com/dusk/40/000000/email.png" alt="Email" />
+            </a>
+            <a href="https://github.com/Greg-Sam">
+
+              <img src="https://img.icons8.com/plasticine/65/000000/github.png" alt="GitHub" />
+              
+            </a>
+            <a href="https://www.linkedin.com/in/greg-samuelson/"> 
+
+              <img src="https://img.icons8.com/doodle/40/000000/linkedin--v2.png" alt="Linkedin" />
+            </a>
+          {/* <a href="https://www.linkedin.com/in/greg-samuelson/"> <MDBIcon fab icon="linkedin" size="2x" /></a> */}
           <hr></hr>
           <h6>Greg is an aspiring full-stack developer with experience in education, project management and international development
                 work. He loves coming up with new ideas and then figuring out how to make them a reality.</h6>
@@ -45,9 +68,20 @@ const Contributors = () => {
           <hr></hr>
           <h4>Kyle Lorenzo</h4>
           <hr></hr>
-          <a href="mailto:kclore3131@gmail.com"><MDBIcon far icon="envelope" size="2x" /></a>
-          <a href="https://github.com/ky-lore"><MDBIcon fab icon="github" size="2x" /></a>
-          <a href="https://www.linkedin.com/in/kyle-lorenzo-46a246161/"> <MDBIcon fab icon="linkedin" size="2x" /></a>
+          {/* <a href="mailto:kclore3131@gmail.com"><MDBIcon far icon="envelope" size="2x" /></a> */}
+          {/* <a href="https://github.com/ky-lore"><MDBIcon fab icon="github" size="2x" /></a> */}
+            <a href="mailto:kclore3131@gmail.com">
+              <img src="https://img.icons8.com/dusk/40/000000/email.png" alt="Email" />
+            </a>
+            <a href="https://github.com/ky-lore">
+
+              <img src="https://img.icons8.com/plasticine/65/000000/github.png" alt="GitHub" />
+            </a>
+            <a href="https://www.linkedin.com/in/kyle-lorenzo-46a246161/">
+
+              <img src="https://img.icons8.com/doodle/40/000000/linkedin--v2.png" alt="Linkedin" />
+            </a>
+          {/* <a href="https://www.linkedin.com/in/kyle-lorenzo-46a246161/"> <MDBIcon fab icon="linkedin" size="2x" /></a> */}
           <hr></hr>
           <h6>Vanilla JS, Node.JS, MySQL, Java developer. Fantasy football failure. Shameless shoujo-manga reader. Go Rams. UCI Coding
                 Bootcamp certification in progress.</h6>
@@ -60,9 +94,20 @@ const Contributors = () => {
             <hr></hr>
           <h4>Joseph Wong Ludwicak</h4>
           <hr></hr>
-          <a href="mailto:josclud@gmail.com"><MDBIcon far icon="envelope" size="2x" /></a>
-          <a href="https://github.com/Josclud"><MDBIcon fab icon="github" size="2x" /></a>
-          <a href="https://www.linkedin.com/in/joseph-wong-ludwicak/"> <MDBIcon fab icon="linkedin" size="2x" /></a>
+          {/* <a href="mailto:josclud@gmail.com"><MDBIcon far icon="envelope" size="2x" /></a> */}
+          {/* <a href="https://github.com/Josclud"><MDBIcon fab icon="github" size="2x" /></a> */}
+            <a href="mailto:josclud@gmail.com">
+              <img src="https://img.icons8.com/dusk/40/000000/email.png" alt="Email" />
+            </a>
+            <a href="https://github.com/Josclud">
+
+              <img src="https://img.icons8.com/plasticine/65/000000/github.png" alt="GitHub" />
+            </a>
+            <a href="https://www.linkedin.com/in/joseph-wong-ludwicak/">
+
+              <img src="https://img.icons8.com/doodle/40/000000/linkedin--v2.png" alt="Linkedin" />
+            </a>
+          {/* <a href="https://www.linkedin.com/in/joseph-wong-ludwicak/"> <MDBIcon fab icon="linkedin" size="2x" /></a> */}
           <hr></hr>
           <h6>Hi, Im Joseph and I am currently working towards a career as a full-stack web developer. Checkout my Linkedin and Github
                 to learn more about me and projects</h6>
@@ -75,10 +120,21 @@ const Contributors = () => {
           <hr></hr>
           <h4>Wettim Linton</h4>
           <hr></hr>
-          <a href="mailto:wettimlinton123@gmail.com"><MDBIcon far icon="envelope" size="2x" /></a>
-          <a href="https://github.com/wlinton1"><MDBIcon fab icon="github" size="2x" /></a>
-          <a href="https://www.linkedin.com/in/wettim-linton-54b4421a6/"> <MDBIcon fab icon="linkedin" size="2x" /></a>
-          <hr></hr>
+          {/* <a href="mailto:wettimlinton123@gmail.com"><MDBIcon far icon="envelope" size="2x" /></a> */}
+          {/* <a href="https://github.com/wlinton1"><MDBIcon fab icon="github" size="2x" /></a> */}
+            <a href="mailto:wettimlinton123@gmail.com">
+              <img src="https://img.icons8.com/dusk/40/000000/email.png" alt="Email" />
+            </a>
+            <a href="https://github.com/wlinton1">
+
+              <img src="https://img.icons8.com/plasticine/65/000000/github.png" alt="GitHub" />
+            </a>
+            <a href="https://www.linkedin.com/in/wettim-linton-54b4421a6/"> 
+
+              <img src="https://img.icons8.com/doodle/40/000000/linkedin--v2.png" alt="Linkedin" />
+            </a>
+          {/* <a href="https://www.linkedin.com/in/wettim-linton-54b4421a6/"> <MDBIcon fab icon="linkedin" size="2x" /></a>
+          <hr></hr> */}
           <h6>I'm currently enrolled in the UCI Full Stack Coding Bootcamp. I'm looking forward to
           learning all there is to know about web development in order to create fantastic websites. We are 7 weeks in and
           I am currently versed in HTML, CSS, JS, node and mysql</h6>
